70c7917f04c75810fb1f018d5a925995bf5ba390
Konstantin Dorichev <kdorichev@gmail.com>
2019-01-02 17:31:06 +0300

    Initial skeleton

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An image classifier using fast.ai over PyTorch.',
+    author='Konstantin Dorichev',
+    license='MIT',
+)
